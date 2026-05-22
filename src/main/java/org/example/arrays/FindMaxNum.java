8644101f26948c1ca62ac71eb7cc38f33963157e
sahil afrid farookhi <msafarookhi@gmail.com>
2026-05-21 23:11:12 -0500

    add find max number

--- src/main/java/org/example/arrays/FindMaxNum.java
+++ src/main/java/org/example/arrays/FindMaxNum.java
@@ -2,6 +2,25 @@ package org.example.arrays;
 
 public class FindMaxNum {
     public static void main (String[] args) {
+        int[] numbers = {9, 3, 11, 7, 2};
 
+        System.out.print("Give numbers: ");
+        for (int i : numbers) {
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\nThe largest number is: " + findMax(numbers));
+    }
+
+    public static int findMax (int[] numbers) {
+        int max = numbers[0];
+
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+
+        return max;
     }
 }
